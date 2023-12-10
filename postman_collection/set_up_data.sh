c3d49e68f20b5770267a214387dd2be11542682e
Евгений Москалянов <emos@inbox.ru>
2023-12-10 21:59:42 +0300

    Исправил ошибку в скрипте.

--- postman_collection/set_up_data.sh
+++ postman_collection/set_up_data.sh
@@ -8,6 +8,7 @@ esac
 
 cd ../
 echo "from yacut import db; \
+     from yacut.models import URLMap; \
      db.create_all(); \
      URLMap.query.delete(); \
      url_map_object = URLMap(original='https://example.com/', short='example'); db.session.add(url_map_object); db.session.commit()" | flask shell >/dev/null 2>&1
