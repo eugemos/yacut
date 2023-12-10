6716bb623510801b5696789f6cfa734accb470f2
Евгений Москалянов <emos@inbox.ru>
2023-12-10 22:30:34 +0300

    Рефакторинг

--- yacut/views.py
+++ yacut/views.py
@@ -11,24 +11,25 @@ from .models import URLMap
 def create_short_link_view():
     form = URLMapForm()
     if form.validate_on_submit():
+        url_map = URLMap(original=form.original_link.data)
         custom_id = form.custom_id.data
         if custom_id is not None:
             custom_id = custom_id.strip()
-        if custom_id and URLMap.query.filter_by(short=custom_id).first():
-            flash(
-                'Предложенный вариант короткой ссылки уже существует.',
-                category='error'
-            )
-            return render_template('create_short_link.html', form=form)
-        url_map = URLMap(
-            original=form.original_link.data, 
-            short=custom_id or None, 
-        )
+        if custom_id:
+            if URLMap.query.filter_by(short=custom_id).first():
+                flash(
+                    'Предложенный вариант короткой ссылки уже существует.',
+                    category='error'
+                )
+                return render_template('create_short_link.html', form=form)
+            url_map.short = custom_id
+
         db.session.add(url_map)
         db.session.commit()
         flash('Ваша новая ссылка готова:')
         flash(f'{request.base_url}{url_map.short}', category='link')
         return render_template('create_short_link.html', form=form)
+        
     return render_template('create_short_link.html', form=form)
 
 
