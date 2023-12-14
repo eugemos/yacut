32b157788519895058f14487665d18009637e63e
Евгений Москалянов <emos@inbox.ru>
2023-12-14 19:47:56 +0300

    Ещё комментарии.

--- yacut/exceptions.py
+++ yacut/exceptions.py
@@ -1,18 +1,24 @@
 class ProgramError(Exception):
+    """Базовый класс всех собственных исключений программы."""
     pass
 
 
 class InvalidInputData(ProgramError):
+    """Возникает при валидации входных данных."""
     def __init__(self, message):
         super().__init__()
         self.message = message
 
 
 class ShortLinkAlreadyExists(ProgramError):
+    """Возникает, когда предложенный пользователем вариант короткой ссылки
+    уже существует.
+    """
     message = 'Предложенный вариант короткой ссылки уже существует.'
 
 
 class InvalidAPIUsage(ProgramError):
+    """Возникает, когда какие-либо ошибки возникают при обращении к API."""
     status_code = 400
 
     def __init__(self, message, status_code=None):
