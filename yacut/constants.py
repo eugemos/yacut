89633ff70545692f283788167abd6c9127accf1b
emos <emos@ntk2.local>
2023-12-14 16:20:33 +0300

    Рефакторинг после ревью закончен.

--- yacut/constants.py
+++ yacut/constants.py
@@ -1,6 +1,7 @@
-SHORT_LINK_DEFAULT_CHARS = (
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-)
+import string
+
+
+SHORT_LINK_DEFAULT_CHARS = string.ascii_letters + string.digits
 SHORT_LINK_DEFAULT_LENGTH = 6
 SHORT_LINK_MAX_LENGTH = 16
 SHORT_LINK_REGEXP = r'\s*[_0-9a-zA-Z]{1,16}\s*$'
